ed10ac05e4bd27f1a8da6eb58caa984d714cf77b
Siddesh <siddeshsankhya@gmail.com>
2024-03-12 19:57:53 +0530

    tweaks

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -44,9 +44,9 @@ export async function POST(req: any) {
           <meta name="fc:frame" content="vNext" />
           <meta name="fc:frame:post_url" content="${process.env.NEXT_PUBLIC_HOST}/api/frame" />
           <meta name="fc:frame:image" content="${image_url}" />
-          <meta name="fc:frame:button:1" content="Subscribe" />
+          <meta name="fc:frame:button:1" content="Visit Push Dapp" />
           <meta name="fc:frame:button:1:action" content="link" />
-          <meta name=fc:frame:button:1:target" content="app.push.org" />
+          <meta name=fc:frame:button:1:target" content="https://app.push.org" />
         
         </head>
         <body/>
