0bde4b7496f7db0219cae9fddfc00fe852b7a5da
Siddesh <siddeshsankhya@gmail.com>
2024-05-15 16:04:20 +0530

    tweak api

--- app/api/register-frame/route.ts
+++ app/api/register-frame/route.ts
@@ -3,49 +3,51 @@ import Frame from "@/models/frame";
 import {headers} from "next/headers";
 import {NextResponse} from "next/server";
 
-export async function POST(req: any) {
-  const {owner, command, url, description} = await req.json();
-  const apiKey = headers().get("x-api-key");
-  //enforece some api rules, temporarily using jws token for owner
+interface FrameRequest {
+  owner: string;
+  command: string;
+  url: string;
+  description?: string;
+}
 
-  if (apiKey !== process.env.JWS_SECRET) {
-    return NextResponse.json({error: "unauthorized", status: 401});
-  }
-
-  if (!owner || !command || !url) {
-    return NextResponse.json({error: "missing fields"});
-  }
-
+async function handleDatabaseConnection() {
   try {
     await connectToDatabase();
-
-    const isCommandExists = await Frame.findOne({command});
-
-    if (isCommandExists) {
-      return NextResponse.json({error: "command already exists", status: 400});
-    }
-
-    const frame = await Frame.create({owner, command, url, description});
-    return NextResponse.json(frame);
   } catch (error) {
-    return NextResponse.json({error, status: 500});
+    console.error("Database connection error:", error);
+    return NextResponse.json({error: "Internal Server Error", status: 500});
   }
 }
 
-export async function GET() {
-  try {
-    await connectToDatabase();
-    const frames = await Frame.find(
-      {},
-      {
-        __v: 0,
-        _id: 0,
-        createdAt: 0,
-        updatedAt: 0,
-      }
-    );
-    return NextResponse.json(frames);
-  } catch (error) {
-    return NextResponse.json({error, status: 500});
+export async function POST(req: any) {
+  const {owner, command, url, description}: FrameRequest = await req.json();
+  const apiKey = headers().get("x-api-key");
+
+  if (apiKey !== process.env.JWS_SECRET) {
+    return NextResponse.json({error: "Unauthorized", status: 401});
   }
+
+  if (!owner || !command || !url) {
+    return NextResponse.json({error: "Missing fields", status: 400});
+  }
+
+  await handleDatabaseConnection();
+
+  const isCommandExists = await Frame.findOne({command});
+  if (isCommandExists) {
+    return NextResponse.json({error: "Command already exists", status: 400});
+  }
+
+  const frame = await Frame.create({owner, command, url, description});
+  return NextResponse.json(frame);
+}
+
+export async function GET() {
+  await handleDatabaseConnection();
+
+  const frames = await Frame.find(
+    {},
+    {__v: 0, _id: 0, createdAt: 0, updatedAt: 0}
+  );
+  return NextResponse.json(frames);
 }
